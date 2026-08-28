8e5a9df2db9fba15f08eee5ed1549b83e558341b
Mynor Meza <mynormeza92@gmail.com>
2026-08-28 10:38:33 +0200

    fix format in build-logic

--- build-logic/convention/src/main/kotlin/PaparazziConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/PaparazziConventionPlugin.kt
@@ -3,7 +3,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 
-class PaparazziConventionPlugin: Plugin<Project> {
+class PaparazziConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             pluginManager.apply("app.cash.paparazzi")
